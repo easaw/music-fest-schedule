4798840938a7760cf6aa798db253d05c01de9e24
= <payne.robin@gmail.com>
2016-03-29 14:23:17 -0600

    Added clock component

--- app/component/App.jsx
+++ app/component/App.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import Schedule from './Schedule.jsx';
+import Clock from './Clock.jsx';
 import Time from  '../static/times.js';
-
-const data = Time();
 
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+                  data: Time(),
+                  currentTime: Date.now()
+                  };
+  }
+
+  setTime = () => {
+    this.setState({
+      currentTime: Date.now()
+    });
+  }
+
+  componentDidMount(){
+    this.timeOutId =
+    window.setTimeOut(
+      this.setTime(), 60000
+    );
+
+  }
+
   render() {
-    return <Schedule data={data} />;
+    debugger;
+    return (
+      <div>
+        <Clock
+          time={this.state.currentTime}
+        />
+        <Schedule
+          data={this.state.currentTime}
+          time={this.state.data}
+        />
+      </div>
+    );
   }
 }
---
+++ app/component/Clock.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class Schedule extends React.Component{
+  render(){
+    return <div>The clock!</div>;
+  }
+
+}
