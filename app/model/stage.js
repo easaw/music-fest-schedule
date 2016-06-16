33ffced8f3b94ea2d450c45c3a8db20b1bd547a0
Robin <payne.robin@gmail.com>
2016-06-16 17:20:38 -0600

    Added Date to stage

--- app/model/stage.js
+++ app/model/stage.js
@@ -1,9 +1,11 @@
 import uuid from 'node-uuid';
+import moment from 'moment';
 
 class Stage{
-  constructor(name = "Name a Stage"){
+  constructor(name = "Name a Stage", date = moment()){
     this.id = uuid.v4();
-    this.name = name;
+    this.date = date,
+    this.name = name,
     this.acts = [];
   }
 }
