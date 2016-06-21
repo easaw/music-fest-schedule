7b5d570c67a1090af1df72f4566a3c270a6a359c
Robin <payne.robin@gmail.com>
2016-06-21 00:14:16 -0600

    Two way

--- app/model/act.js
+++ app/model/act.js
@@ -4,11 +4,13 @@ import moment from 'moment';
 class Act{
   constructor(
     dj = "Create a Name",
-    start = moment(),
-    end = moment() )
+    start = moment().format(),
+    end = moment().format(),
+    stageId )
     {
       this.dj = dj;
       this.id = uuid.v4();
+      this.stageId = stageId;
       this.start = start;
       this.end = end;
   }
--- app/static/state.js
+++ app/static/state.js
@@ -8,87 +8,99 @@ import {
 
 export default (() => {
 
-  const act = new Schema('acts', {
-      idAttribue: 'id'
-  });
+    const act = new Schema('acts', {
+        idAttribue: 'id'
+    });
 
-  const stage = new Schema('stages', {
-      idAttribue: 'id'
-  });
+    const stage = new Schema('stages', {
+        idAttribue: 'id'
+    });
 
-  stage.define({
-      acts: arrayOf(act)
-  });
+    stage.define({
+        acts: arrayOf(act)
+    });
 
-
-  const result = {
-      stages: [{
-          id: uuid.v4(),
-          name: 'Energy',
-          date: '2016-02-13',
-          acts: [{
-              id: uuid.v4(),
-              dj: 'Michael Constable B2B Theo Hansen',
-              start: '2016-02-13T20:00:00-07:00',
-              end: '2016-02-13T21:30:00-07:00'
-          }, {
-              id: uuid.v4(),
-              dj: 'Wicked Science',
-              start: '2016-02-13T21:30:00-07:00',
-              end: '2016-02-13T22:45:00-07:00'
-          }, {
-              id: uuid.v4(),
-              dj: 'ANEVO',
-              start: '2016-02-13T22:45:00-07:00',
-              end: '2016-02-14T00:00:00-07:00'
-          }, {
-              id: uuid.v4(),
-              dj: 'KSHMR',
-              start: '2016-02-14T00:00:00-07:00',
-              end: '2016-02-14T01:15:00-07:00'
-          }, {
-              id: uuid.v4(),
-              dj: 'Dimitri Vegas & Like Mike',
-              start: '2016-02-14T01:15:00-07:00',
-              end: '2016-02-14T02:30:00-07:00'
-          }]
-      }, {
-          id: uuid.v4(),
-          name: 'Bass',
-          date: '2016-02-13',
-          acts: [{
-              id: uuid.v4(),
-              dj: 'Pill Dickle',
-              start: '2016-02-13T20:00:00-07:00',
-              end: '2016-02-13T21:00:00-07:00'
-          }, {
-              id: uuid.v4(),
-              dj: 'Random Hero B2B Jabonesio',
-              start: '2016-02-13T21:00:00-07:00',
-              end: '2016-02-13T22:00:00-07:00'
-          }, {
-              id: uuid.v4(),
-              dj: 'Flatland funk',
-              start: '2016-02-13T22:00:00-07:00',
-              end: '2016-02-13T23:00:00-07:00'
-          }, {
-              id: uuid.v4(),
-              dj: 'Aero Chord',
-              start: '2016-02-13T23:00:00-07:00',
-              end: '2016-02-14T00:00:00-07:00'
-          }, {
-              id: uuid.v4(),
-              dj: 'Snails',
-              start: '2016-02-14T00:00:00-07:00',
-              end: '2016-02-14T01:30:00-07:00'
-          }, {
-              id: uuid.v4(),
-              dj: 'Feed Me',
-              start: '2016-02-14T01:30:00-07:00',
-              end: '2016-02-14T02:45:00-07:00'
-          }]
-      }]
-  };
+    const stage1Id = uuid.v4();
+    const stage2Id = uuid.v4();
+    const result = {
+        stages: [{
+            id: stage1Id,
+            name: 'Energy',
+            date: '2016-02-13',
+            acts: [{
+                id: uuid.v4(),
+                stageId: stage1Id,
+                dj: 'Michael Constable B2B Theo Hansen',
+                start: '2016-02-13T20:00:00-07:00',
+                end: '2016-02-13T21:30:00-07:00'
+            }, {
+                id: uuid.v4(),
+                stageId: stage1Id,
+                dj: 'Wicked Science',
+                start: '2016-02-13T21:30:00-07:00',
+                end: '2016-02-13T22:45:00-07:00'
+            }, {
+                id: uuid.v4(),
+                stageId: stage1Id,
+                dj: 'ANEVO',
+                start: '2016-02-13T22:45:00-07:00',
+                end: '2016-02-14T00:00:00-07:00'
+            }, {
+                id: uuid.v4(),
+                stageId: stage1Id,
+                dj: 'KSHMR',
+                start: '2016-02-14T00:00:00-07:00',
+                end: '2016-02-14T01:15:00-07:00'
+            }, {
+                id: uuid.v4(),
+                stageId: stage1Id,
+                dj: 'Dimitri Vegas & Like Mike',
+                start: '2016-02-14T01:15:00-07:00',
+                end: '2016-02-14T02:30:00-07:00'
+            }]
+        }, {
+            id: stage2Id,
+            name: 'Bass',
+            date: '2016-02-13',
+            acts: [{
+                id: uuid.v4(),
+                stageId: stage2Id,
+                dj: 'Pill Dickle',
+                start: '2016-02-13T20:00:00-07:00',
+                end: '2016-02-13T21:00:00-07:00'
+            }, {
+                id: uuid.v4(),
+                stageId: stage2Id,
+                dj: 'Random Hero B2B Jabonesio',
+                start: '2016-02-13T21:00:00-07:00',
+                end: '2016-02-13T22:00:00-07:00'
+            }, {
+                id: uuid.v4(),
+                stageId: stage2Id,
+                dj: 'Flatland funk',
+                start: '2016-02-13T22:00:00-07:00',
+                end: '2016-02-13T23:00:00-07:00'
+            }, {
+                id: uuid.v4(),
+                stageId: stage2Id,
+                dj: 'Aero Chord',
+                start: '2016-02-13T23:00:00-07:00',
+                end: '2016-02-14T00:00:00-07:00'
+            }, {
+                id: uuid.v4(),
+                stageId: stage2Id,
+                dj: 'Snails',
+                start: '2016-02-14T00:00:00-07:00',
+                end: '2016-02-14T01:30:00-07:00'
+            }, {
+                id: uuid.v4(),
+                stageId: stage2Id,
+                dj: 'Feed Me',
+                start: '2016-02-14T01:30:00-07:00',
+                end: '2016-02-14T02:45:00-07:00'
+            }]
+        }]
+    };
 
     let results = normalize(result, {
         stages: arrayOf(stage)
