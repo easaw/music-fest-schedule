78e7ff3d0f3ae68ba6eb08d6b8ad81ceb7817994
Robin <payne.robin@gmail.com>
2016-06-21 23:41:46 -0600

    Persistent Storage

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,9 @@
 import './styles/main.less';
 import React from 'react';
 import {render} from 'react-dom';
-import {createStore, combineReducers} from 'redux';
+import {compose, createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
+import persistState from 'redux-localstorage'
 import {Router, Route, browserHistory} from 'react-router';
 import {syncHistoryWithStore, routerReducer} from 'react-router-redux';
 import * as reducers from './reducers/index.js';
@@ -11,13 +12,18 @@ import Stage from './routes/Stage/Index.jsx';
 import Act from './routes/Act/Index.jsx';
 import State from './static/state.js';
 
+
+const paths = ['stages', 'acts'];
+const enhancer = compose(
+  persistState(paths, /*config*/),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+
 const state = State;
 const store = createStore(combineReducers({
     ...reducers,
     routing: routerReducer
-}), state, window.devToolsExtension
-    ? window.devToolsExtension()
-    : undefined);
+}), state, enhancer);
 
 const history = syncHistoryWithStore(browserHistory, store);
 
