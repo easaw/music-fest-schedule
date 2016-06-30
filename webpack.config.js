ecbb219c74cc39b1d0aa84c708842247dce823c1
Robin <payne.robin@gmail.com>
2016-06-29 20:12:58 -0600

    RTFMing and fixing my issues

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const validate = require('webpack-validator');
+const autoprefixer = require('autoprefixer');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
@@ -14,17 +15,27 @@ const PATHS = {
 process.env.BABEL_ENV = TARGET;
 
 const common = {
-    entry: {
-        app: PATHS.app
-    },
+    entry: [
+        'webpack-hot-middleware/client',
+        'tether',
+        'font-awesome-loader',
+        'bootstrap-loader',
+        PATHS.app
+    ],
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
     output: {
         path: PATHS.build,
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+          publicPath: '/assets/'
     },
-    plugins: [],
+    plugins: [new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.ProvidePlugin({
+            "window.Tether": "tether"
+        })
+    ],
     module: {
         loaders: [{
             test: /\.jsx?$/,
@@ -35,9 +46,15 @@ const common = {
             exclude: /node_modules/,
             loader: 'babel-loader'
         }, {
-        test: /(\.scss|\.css)$/,
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
-      }]
+            test: /(\.scss|\.css)$/,
+            loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
+        }, {
+            test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "url?limit=10000"
+        }, {
+            test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+            loader: 'file'
+        }, ]
     },
 };
 
@@ -59,22 +76,26 @@ const devExports = {
         port: process.env.PORT
     },
     plugins: [
-        new ExtractTextPlugin('react-toolbox.css', { allChunks: true }),
+        new ExtractTextPlugin('react-toolbox.css', {
+            allChunks: true
+        }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin(),
         new NpmInstallPlugin({
             save: true // --save
         })
-    ]
+    ],
+    postcss: [autoprefixer]
+
 };
 
 let exported = {};
 
 if (TARGET === 'start' || !TARGET) {
-    exported = merge(common, devExports );
+    exported = merge(common, devExports);
     validate(exported);
 } else if (TARGET === 'build') {
-  exported = merge(common, {});
+    exported = merge(common, {});
 }
 
 module.exports = exported;
